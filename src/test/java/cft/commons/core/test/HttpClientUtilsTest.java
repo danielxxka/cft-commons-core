9e77f2a7146e1f85471d18ab9fae1a30f2082102
daniel <hk96333@gmail.com>
2014-03-08 01:09:34 +0800

    updated testing

--- src/test/java/cft/commons/core/test/HttpClientUtilsTest.java
+++ src/test/java/cft/commons/core/test/HttpClientUtilsTest.java
@@ -1,5 +1,6 @@
 package cft.commons.core.test;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,22 +13,24 @@ import cft.commons.core.util.HttpClientUtils;
  *
  */
 public class HttpClientUtilsTest {
-	
-	
-		@SuppressWarnings("unused")
-		@Test
-		public void httpPost() throws Exception{
+
+	//@Test
+	public void httpGet() throws IOException {
+			System.out.println("Content = " + HttpClientUtils.httpGet("http://www.baidu.com", 3000, 5000));
+	}
+
+	@Test
+	public void httpPost() throws Exception {
 
 		String xmlString = "<bean id=\"rptspmb002\" class=\"org.springframework.web.servlet.view.jasperreports.JasperReportsMultiFormatView\">"
 				+ "<property name=\"url\" value=\"/WEB-INF/classes/rpt/rptspmb001/rptspmb001.jasper\" /></bean> ";
-		
-		Map<String,String> map = new HashMap<String,String>();
-		//map.put("deviceId", "ABCD");
+
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("xml", xmlString);
 		map.put("deviceToken", "@#%@#^@!#^@!#!%%!");
-		//map.put("createDate", "2013-10-01 11:23:00");
-		
-		
-		System.out.println(HttpClientUtils.httpPost(map, "http://localhost/api/test", 5000, 5000));
+		map.put("createDate", "2013-10-01 11:23:00");
+
+		System.out.println(HttpClientUtils.httpPost(map, "http://localhost/api/test", 2000, 5000));
 
 	}
 }
